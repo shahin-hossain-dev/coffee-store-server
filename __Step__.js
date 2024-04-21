16c1cc47f2c0d271285b8f9ff083e74e1c3e59b1
shahin-hossain-dev <shahin.hossain.dev@gmail.com>
2024-04-21 19:53:48 +0600

    56-2 Client Side Router And Connect MongoDB Database, dovenv

---
+++ __Step__.js
@@ -0,0 +1,10 @@
+/**
+ * Install express, mongo, cors, dotenv
+ * setup 1st basic server
+ *
+ * crate mongo new user for this server
+ * take username password  / make safe
+ * connect with mongodb
+ *
+ *
+ */
