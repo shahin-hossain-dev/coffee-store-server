595f56091e1b9b644919f87729ff6cd46f5c69de
shahin-hossain-dev <shahin.hossain.dev@gmail.com>
2024-04-21 23:46:24 +0600

    56-5 Send Coffee Data To The Server And Store In The Mongodb Database

--- index.js
+++ index.js
@@ -29,6 +29,21 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
+
+    const coffeeCollection = client.db("coffeeDB").collection("coffee");
+
+    app.post("/coffee", async (req, res) => {
+      const coffee = req.body;
+      const result = await coffeeCollection.insertOne(coffee);
+      res.send(result);
+    });
+
+    app.get("/coffee", async (req, res) => {
+      const cursor = coffeeCollection.find();
+      const result = await cursor.toArray();
+      res.send(result);
+    });
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log(
